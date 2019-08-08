80e3f2ad5785cc589091550cb4515fa7dd08a9b6
Punith <punith.ch@gmail.com>
2019-08-07 20:22:05 -0600

    refactored: changed login and contact view and added more details

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,15 +5,19 @@ export default class Login extends React.Component {
     render() {
         return (
             <div className="section-form">
-                <div className="form-container-login">
-                    <div className="section1">
-                        <h3>Log in</h3>
+                <div className="login-container">
+                    <div className="login-left">
+                        <h2>Log in</h2>
                         <input placeholder="Username"></input>
                         <input placeholder="Password"></input>
-                        <a href="#">Login</a>
-                    </div>
-                    <div className="section2">
+                        <div className="btn-container">
+                            <a href="#" className="btn"><i class="fas fa-sign-in-alt"></i>Log in</a>
+                        </div>
 
+                    </div>
+                    <div className="login-right">
+                        <h1>Admin Access Only</h1>
+                        <i class="fas fa-user-shield"></i>
                     </div>
                 </div>
 
